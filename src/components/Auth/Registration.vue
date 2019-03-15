f0c3b51ce2c25453340409a9903e69d50b369eb5
Timur Melnikov <melnikovt@gmail.com>
2019-03-15 11:34:05 +0200

    Создание страницы регистрации

--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -1,17 +1,96 @@
 <template>
-    <v-container>
-        <v-layout row>
-            <v-flex xs12>
-                <h1>Registration</h1>
+    <v-container fill-height fluid>
+        <v-layout align-center justify-center>
+            <v-flex md4 sm8 xs12>
+                <v-card class="elevation-12">
+                    <v-toolbar color="primary" dark>
+                        <v-toolbar-title>Registration form</v-toolbar-title>
+                    </v-toolbar>
+                    <v-card-text>
+                        <v-form ref="form" v-model="valid" lazy-validation>
+                            <v-text-field
+                                    label="E-mail"
+                                    name="email"
+                                    prepend-icon="person"
+                                    type="email"
+                                    v-model="email"
+                                    :rules="emailRules"
+                            >
+                            </v-text-field>
+                            <v-text-field
+                                    label="Password"
+                                    name="password"
+                                    prepend-icon="lock"
+                                    type="password"
+                                    v-model="password"
+                                    :counter="6"
+                                    :rules="passwordRules"
+                            >
+                            </v-text-field>
+                            <v-text-field
+                                    label="Confirm password"
+                                    name="confirm-password"
+                                    prepend-icon="lock"
+                                    type="password"
+                                    v-model="confirmPassword"
+                                    :counter="6"
+                                    :rules="confirmPasswordRules"
+                            >
+                            </v-text-field>
+                        </v-form>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn
+                                color="primary"
+                                @click="onSubmit()"
+                                :disabled="!valid"
+                        >Create account
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
             </v-flex>
         </v-layout>
     </v-container>
 </template>
 
 <script>
+    const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
     export default {
       data () {
-        return {}
+        return {
+          email: '',
+          password: '',
+          confirmPassword: '',
+          valid: false,
+          emailRules: [
+            v => !!v || 'E-mail is required',
+            v => emailRegex.test(v) || 'E-mail must be valid'
+          ],
+          passwordRules: [
+            v => !!v || 'Password is required',
+            v => (v && v.length >= 6) || 'Password must be equal or more than 6 characters'
+          ],
+          confirmPasswordRules: [
+            v => !!v || 'Cjnfirm password is required',
+            v => v === this.password || 'Passwords should matsh'
+          ]
+
+        }
+      },
+      methods: {
+        onSubmit () {
+          if (this.$refs.form.validate()) {
+                    // alert('werwer')
+            const user = {
+              email: this.email,
+              password: this.password,
+              confirmPassword: this.confirmPassword
+
+            }
+            console.log(user)
+          }
+        }
       }
     }
 </script>
