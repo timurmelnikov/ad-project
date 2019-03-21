68e6b9beecc98bcac7ffd58318b2fd954891341c
Timur Melnikov <melnikovt@gmail.com>
2019-03-21 13:10:05 +0200

    Создание страницы объявления

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -2,7 +2,21 @@
     <v-container>
         <v-layout row>
             <v-flex xs12>
-                <h1>Ad</h1>
+                <v-card>
+                    <v-card-media
+                            src="https://v1.vuetifyjs.com/static/doc-images/carousel/bird.jpg"
+                            height="300px"
+                    ></v-card-media>
+                    <v-card-text>
+                        <h1 class="text--primary">1orem</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, repellendus.</p>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn class="warning" flat>Edit</v-btn>
+                        <v-btn class="success">Buy</v-btn>
+                    </v-card-actions>
+                </v-card>
             </v-flex>
         </v-layout>
     </v-container>
