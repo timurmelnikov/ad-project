1af7ad9e7340c51573fdc137d0438f2fa3fc9cd5
Timur Melnikov <melnikovt@gmail.com>
2019-03-27 12:00:53 +0200

    Спрятал данные для подключению к Firebase

---
+++ src/fb_cfg_example.js
@@ -0,0 +1,13 @@
+/*
+Пример конфигурации Firebase
+Файл заполнить правильными данными, скопировать,
+копию переименовать в fb_cfg.js
+ */
+export const config = {
+  apiKey: 'XXXXXXXXXXXXXXXXXXXXXXX',
+  authDomain: 'XXXXXXXXXXXXXXXXXXXXXXX',
+  databaseURL: 'XXXXXXXXXXXXXXXXXXXXXXX',
+  projectId: 'XXXXXXXXXXXXXXXXXXXXXXX',
+  storageBucket: 'XXXXXXXXXXXXXXXXXXXXXXX',
+  messagingSenderId: 'XXXXXXXXXXXXXXXXXXXXXXX'
+}
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import store from './store'
 import * as fb from 'firebase'
+import * as cfg from './fb_cfg'
 
 Vue.use(Vuetify)
 
@@ -18,14 +19,6 @@ new Vue({
   template: '<App/>',
   store,
   created () {
-    const config = {
-      apiKey: 'AIzaSyDop2Pm1MifPhEq898KQvP-3hKrIjI5TdA',
-      authDomain: 'itc-ads-986a4.firebaseapp.com',
-      databaseURL: 'https://itc-ads-986a4.firebaseio.com',
-      projectId: 'itc-ads-986a4',
-      storageBucket: 'itc-ads-986a4.appspot.com',
-      messagingSenderId: '400160213250'
-    }
-    fb.initializeApp(config)
+    fb.initializeApp(cfg.config)
   }
 })
